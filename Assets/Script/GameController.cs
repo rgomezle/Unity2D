7956a4fc879b97045471050b04c1b314e0d339c9
rgomezle <rgomez.leyton@gmail.com>
2017-09-13 16:50:17 -0300

    Fondo, Parallax, Sprite

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	[Range (0f, 0.20f)]
+	public float parallaxSpeed = 0.02f;
+	public RawImage background;
+	public RawImage platform;
+
+	public GameObject uiParado;
+
+	//Enumerador, lista de opciones
+	public enum GameState {Parado, Jugando};
+	//Por defecto el juego está parado
+	public GameState gameState = GameState.Parado;
+
+	void Start () {
+		
+	}
+	void Update () {
+
+		//Detecta el inicio del Juego
+		if (gameState == GameState.Parado && (Input.GetKeyDown ("up") || Input.GetMouseButtonDown (0))) {
+			gameState = GameState.Jugando;
+			uiParado.SetActive (false);
+
+		} else if (gameState == GameState.Jugando) {
+			Parallax ();
+		}
+		}
+
+	void Parallax(){
+	
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		background.uvRect = new Rect (background.uvRect.x + finalSpeed * 2, 0f, 1f, 1f);
+		platform.uvRect = new Rect (platform.uvRect.x + finalSpeed * 8, 0f, 1f, 1f);
+	
+	}
+
+}
