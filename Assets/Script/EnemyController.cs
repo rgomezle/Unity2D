54662fe0898414cf56faf76e504ce239cf5ba4a8
rgomezle <rgomez.leyton@gmail.com>
2017-09-29 11:57:07 -0300

    Add enemy

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float velocity = 2f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
